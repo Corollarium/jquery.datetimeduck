25e473f709c992c9c5dcc259f6f5b37500e7b674
brunobg <brunobg@gmail.com>
2017-10-11 12:49:02 -0300

    Avoid a second reload

--- jquery.datetimeduck/jquery.datetimeduck.js
+++ jquery.datetimeduck/jquery.datetimeduck.js
@@ -245,7 +245,7 @@
 			}
 		}
 
-		if (showTime) {
+		if (showTime && inst.dpDiv.find('.datepicker-time').length == 0) {
 			var table = inst.dpDiv.find('table:last');
 			var thead = table.find('thead>tr:first');
 			var tbody = table.find('tbody>tr:first');
